bc85b63b7205af46098f807a2fdcd2b9a0ba1911
Théo HYVON <theo.p-h@live.fr>
2025-02-03 17:49:16 +0100

    Adding main Task Vehicle

---
+++ CM7/Core/Src/tasks/VehicleTask.cpp
@@ -0,0 +1,44 @@
+/*
+ * VehicleTask.cpp
+ *
+ *  Created on: Feb 3, 2025
+ *      Author: To
+ */
+
+#include "VehicleTask.h"
+
+VehicleTask Vehicle;
+
+VehicleTask::VehicleTask()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+void VehicleTask::setup()
+{
+	CanHandler.start("CAN", 256, osPriorityHigh);
+	MainController.start("PS3", 128, osPriorityAboveNormal);
+}
+
+void VehicleTask::run()
+{
+	osDelay(50);
+	PS3::Data controller = MainController.getData();
+	if(controller.buttons.cross)
+	{
+		std::string value = std::to_string(controller.sticks.L.x);
+		value += ",";
+		value += std::to_string(controller.sticks.L.y);
+		value += "\n";
+
+		CDC_Transmit_FS((uint8_t*)value.data(), value.size());
+	}
+
+}
+
+void VehicleTask::cleanup()
+{
+
+}
+
---
+++ CM7/Core/Src/tasks/VehicleTask.h
@@ -0,0 +1,32 @@
+/*
+ * VehicleTask.h
+ *
+ *  Created on: Feb 3, 2025
+ *      Author: To
+ */
+
+#ifndef SRC_TASKS_VEHICLETASK_H_
+#define SRC_TASKS_VEHICLETASK_H_
+
+#include "RTOSTask.h"
+#include "PS3Controller.h"
+#include "usbd_cdc_if.h"
+
+#include <string>
+
+class VehicleTask : public RTOS_Task
+{
+	public:
+		VehicleTask();
+
+		void setup() override;
+		void run() override;
+		void cleanup() override;
+
+	private:
+		PS3Controller mController;
+};
+
+extern VehicleTask Vehicle;
+
+#endif /* SRC_TASKS_VEHICLETASK_H_ */
