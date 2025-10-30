89aa536e6a1405b265bcd8f5f12368bc49c9e19b
Théo HYVON <theo.p-h@live.fr>
2025-10-30 10:14:39 +0100

    Add HandleBar controller task

---
+++ CM7/Core/Src/tasks/HandleBarController.cpp
@@ -0,0 +1,240 @@
+/*
+ * PS3Controller.cpp
+ *
+ *  Created on: Feb 3, 2025
+ *      Author: To
+ */
+
+#include "HandleBarController.h"
+
+HandleBarController HandleBarTask;
+extern ActionPacketPoolHandler ActionPacketPool;
+
+HandleBarController::HandleBarController()
+{
+	setRangeFilter(0x20, 0x29);
+	setCommunicationTimeout(5000);
+	setRecoveryMode(100, 2500);
+
+	mPreviousTick = 0;
+
+	CanHandler.attach(this);
+}
+
+void HandleBarController::init()
+{
+	CanPacket *HandleBarControllerSettings = CanPacketPool.allocate(0x29);
+	HandleBarControllerSettings->data.push_back(0x01);
+	if(CanHandler.send(HandleBarControllerSettings)) //TODO Handle multiple failed init
+	{
+		CanPeripheral::init();
+	}
+}
+
+void HandleBarController::reInit()
+{
+	CanPacket *HandleBarControllerSettings = CanPacketPool.allocate(0x29);
+	HandleBarControllerSettings->data.push_back(0x01);
+	if(CanHandler.send(HandleBarControllerSettings))
+	{
+		// Todo handle full Queue
+	}
+
+}
+
+void HandleBarController::absent()
+{
+	log(Message(Message::LogCritical) << LOG_HANDLEBAR_ABSENT);
+	while(1)
+	{
+		osDelay(10000);
+	}
+}
+
+void HandleBarController::recovery()
+{
+	log(Message(Message::LogError) << LOG_HANDLEBAR_RECOVERY_ATTEMPT);
+	reInit();
+}
+
+void HandleBarController::recovered()
+{
+	log(Message(Message::LogInfo) << LOG_HANDLEBAR_RECOVERED);
+}
+
+void HandleBarController::lost()
+{
+	log(Message(Message::LogError) << LOG_HANDLEBAR_LOST);
+}
+
+void HandleBarController::setup()
+{
+	Mut_Data = xSemaphoreCreateMutex();
+	xSemaphoreGive(Mut_Data);
+	init();
+}
+
+void HandleBarController::run()
+{
+	CanPacket* packet = nullptr;
+	if(xQueueReceive(mPacketsQueue, &packet, 0) == pdTRUE)
+	{
+		if(packet != nullptr)
+		{
+			if(packet->Identifier == 0x20)
+				ControllerData(packet);
+			else if(packet->Identifier == 0x21)
+				ControllerStatus(packet);
+			CanPacketPool.free(packet);
+		}
+	}
+
+	uint32_t dt = xTaskGetTickCount() - mPreviousTick;
+	mPreviousTick = xTaskGetTickCount();
+
+	mBrakeSwitch.tick(dt);
+	mLightsSwitch.tick(dt);
+	mParkBrakeSwitch.tick(dt);
+	mWarningSwitch.tick(dt);
+	mHornSwitch.tick(dt);
+	mTurnLSwitch.tick(dt);
+	mTurnRSwitch.tick(dt);
+	mReverseSwitch.tick(dt);
+
+	tick(xTaskGetTickCount());
+	osDelay(10);
+}
+
+void HandleBarController::ControllerStatus(CanPacket* packet)
+{
+	//Invalid packet
+	if(packet->data.size() < 4)
+		return;
+
+	if(xSemaphoreTake(Mut_Data, 10) == pdTRUE)
+	{
+		heartbeat |= packet->data[0] << 24;
+		heartbeat |= packet->data[1] << 16;
+		heartbeat |= packet->data[2] << 8;
+		heartbeat |= packet->data[3] & 0xFF;
+
+		xSemaphoreGive(Mut_Data);
+	}
+}
+
+void HandleBarController::ControllerData(CanPacket* packet)
+{
+	//Invalid packet
+	if(packet->data.size() < 4)
+		return;
+
+	if(xSemaphoreTake(Mut_Data, 10) == pdTRUE)
+	{
+		mThrottle = uint8_t(packet->data[0]);
+		mBrake = uint8_t(packet->data[1]);
+		mSteering = uint8_t(packet->data[2]) << 8 | uint8_t(packet->data[3]);
+
+		setThrottleCommand(mThrottle);
+		setBrakeCommand(mBrake);
+		setSteeringCommand(mSteering);
+
+		switch(mTurnLSwitch.read())
+		{
+			case Switch::States::PRESSED:
+				setLightsCommand(Action::Signals::Left, true);
+				HAL_GPIO_WritePin(SND_0_GPIO_Port, SND_0_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SND_1_GPIO_Port, SND_1_Pin, GPIO_PIN_RESET);
+				break;
+			case Switch::States::RELEASED:
+				setLightsCommand(Action::Signals::Left, false);
+				HAL_GPIO_WritePin(SND_0_GPIO_Port, SND_0_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SND_1_GPIO_Port, SND_1_Pin, GPIO_PIN_SET);
+				break;
+			default:
+				break;
+		}
+		switch(mTurnRSwitch.read())
+		{
+			case Switch::States::PRESSED:
+				setLightsCommand(Action::Signals::Right, true);
+				HAL_GPIO_WritePin(SND_0_GPIO_Port, SND_0_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SND_1_GPIO_Port, SND_1_Pin, GPIO_PIN_RESET);
+				break;
+			case Switch::States::RELEASED:
+				setLightsCommand(Action::Signals::Right, false);
+				HAL_GPIO_WritePin(SND_0_GPIO_Port, SND_0_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SND_1_GPIO_Port, SND_1_Pin, GPIO_PIN_SET);
+				break;
+			default:
+				break;
+		}
+
+		switch (mWarningSwitch.read())
+		{
+			case Switch::States::PRESSED:
+				setLightsCommand(Action::Signals::Hazard, true);
+				HAL_GPIO_WritePin(SND_0_GPIO_Port, SND_0_Pin, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(SND_1_GPIO_Port, SND_1_Pin, GPIO_PIN_SET);
+				break;
+			case Switch::States::RELEASED:
+				setLightsCommand(Action::Signals::Hazard, false);
+				HAL_GPIO_WritePin(SND_0_GPIO_Port, SND_0_Pin, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SND_1_GPIO_Port, SND_1_Pin, GPIO_PIN_SET);
+				break;
+			default:
+				break;
+		}
+
+/*		switch (controller.buttons.start.read())
+		{
+			case Switch::States::RELEASED:
+				setMotorEngage(true);
+				break;
+			default:
+				break;
+		}
+*/
+		switch (mBrakeSwitch.read())
+		{
+			case Switch::States::PRESSED:
+				setLightsCommand(Action::Signals::BrakeSignal, true);
+				break;
+			case Switch::States::RELEASED:
+				setLightsCommand(Action::Signals::BrakeSignal, false);
+				break;
+			default:
+				break;
+		}
+
+		switch (mLightsSwitch.read())
+		{
+			case Switch::States::PRESSED:
+				setLightsCommand(Action::Signals::NighLight, true);
+				break;
+			case Switch::States::RELEASED:
+				setLightsCommand(Action::Signals::NighLight, false);
+				break;
+			default:
+				break;
+		}
+
+		switch (mReverseSwitch.read())
+		{
+			case Switch::States::PRESSED:
+				setReverseCommand(Action::Gear::Reverse);
+				break;
+			case Switch::States::RELEASED:
+				setReverseCommand(Action::Gear::Forward);
+				break;
+			default:
+				break;
+		}
+
+		xSemaphoreGive(Mut_Data);
+	}
+}
+
+void HandleBarController::cleanup()
+{
+
+}
---
+++ CM7/Core/Src/tasks/HandleBarController.h
@@ -0,0 +1,63 @@
+/*
+ * PS3Controller.h
+ *
+ *  Created on: Feb 3, 2025
+ *      Author: To
+ */
+
+#ifndef SRC_TASKS_PS3CONTROLLER_H_
+#define SRC_TASKS_PS3CONTROLLER_H_
+
+#include "RTOSTask.h"
+#include "queue.h"
+#include "semphr.h"
+#include "utils/CanPeripheral.h"
+#include "CANTask.h"
+#include "utils/Controller.h"
+#include "utils/Switch.h"
+
+
+class HandleBarController: public CanPeripheral, public RTOS_Task, public Controller
+{
+
+	public:
+		HandleBarController();
+
+		void setup() override;
+		void run() override;
+		void cleanup() override;
+
+		void init() override;
+		void reInit();
+		void recovery();
+		void absent();
+		void recovered();
+		void lost();
+
+	private:
+		void ControllerStatus(CanPacket* packet);
+		void ControllerData(CanPacket* packet);
+
+	private:
+		SemaphoreHandle_t Mut_Data;
+		//Peripheral control
+		uint32_t mPreviousTick = 0;
+
+		uint8_t mThrottle;
+		uint8_t mBrake;
+		uint16_t mSteering;
+
+		Switch mBrakeSwitch;
+		Switch mLightsSwitch;
+		Switch mParkBrakeSwitch;
+		Switch mWarningSwitch;
+		Switch mHornSwitch;
+		Switch mTurnLSwitch;
+		Switch mTurnRSwitch;
+		Switch mReverseSwitch;
+
+};
+
+extern HandleBarController HandleBarTask;
+
+#endif /* SRC_TASKS_PS3CONTROLLER_H_ */
