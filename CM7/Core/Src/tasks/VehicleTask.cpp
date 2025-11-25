70422483cee0c91ef18ec315653e56d70dbd2c82
Théo HYVON <theo.p-h@live.fr>
2025-11-25 09:10:15 +0100

    Memory monitoring

--- CM7/Core/Src/tasks/VehicleTask.cpp
+++ CM7/Core/Src/tasks/VehicleTask.cpp
@@ -64,6 +64,7 @@ void VehicleTask::run()
 {
 	Action* action = nullptr;
 
+	freeHeap = xPortGetFreeHeapSize();
 	// Read received action from controller
 	while(xQueueReceive(mControllerQueue, &action, pdMS_TO_TICKS(10)) == pdTRUE)
 	{
