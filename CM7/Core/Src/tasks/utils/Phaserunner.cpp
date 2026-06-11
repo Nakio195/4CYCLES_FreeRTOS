a7860bd6843991fc7fee7281ae6ddcebb95dfdca
Théo HYVON <theo.p-h@live.fr>
2026-06-11 09:13:01 +0200

    Enable brake by current injection

--- CM7/Core/Src/tasks/utils/Phaserunner.cpp
+++ CM7/Core/Src/tasks/utils/Phaserunner.cpp
@@ -222,7 +222,7 @@ void Phaserunner::setSpeedLimit(float limit)
 void Phaserunner::setBrake(float brake)
 {
 	//TODO Filter input and check motor state
-	mMotorCommands.BrakingCurrentLimit = 0;//brake;
+	mMotorCommands.BrakingCurrentLimit = brake;
 }
 
 
